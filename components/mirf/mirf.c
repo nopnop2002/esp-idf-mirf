55282e0bfe8c5f896b9c24ff5418a6b787499271
nopnop2002 <hau26280@tree.odn.ne.jp>
2022-02-22 18:04:09 +0900

    Typo

--- components/mirf/mirf.c
+++ components/mirf/mirf.c
@@ -25,7 +25,7 @@ static const int SPI_Frequency = 4000000; // Stable even with a long jumper cabl
 //static const int SPI_Frequency = 8000000; // Requires a short jumper cable
 //static const int SPI_Frequency = 10000000; // Unstable even with a short jumper cable
 
-const char rf24_datarates[][8] = {"1MBPS", "2MBPS", "250KBPS"};
+const char rf24_datarates[][8] = {"1Mbps", "2Mbps", "250Kbps"};
 const char rf24_crclength[][10] = {"Disabled", "8 bits", "16 bits"};
 const char rf24_pa_dbm[][8] = {"PA_MIN", "PA_LOW", "PA_HIGH", "PA_MAX"};
 
