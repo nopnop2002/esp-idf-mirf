2944d820e647eef03a90320b61061261bf6a4248
nopnop2002 <hau26280@tree.odn.ne.jp>
2022-11-13 16:00:15 +0900

    Added sender example

--- Multiple-Receive/main/main.c
+++ Multiple-Receive/main/main.c
@@ -21,8 +21,10 @@ typedef union {
 	unsigned int ivalue;
 } MYDATA_t;
 
+
 MYDATA_t mydata;
 
+#if CONFIG_RECEIVER
 void receiver(void *pvParameters)
 {
 	ESP_LOGI(pcTaskGetName(0), "Start");
@@ -59,8 +61,61 @@ void receiver(void *pvParameters)
 		vTaskDelay(1);
 	}
 }
+#endif // CONFIG_RECEIVER
+
+#if CONFIG_TRANSMITTER
+void transmitter(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(0), "Start");
+	NRF24_t dev;
+	Nrf24_init(&dev);
+	uint8_t payload = sizeof(mydata.value);
+	uint8_t channel = 90;
+	Nrf24_config(&dev, channel, payload);
+
+	//Set the receiver address using 5 characters
+#if CONFIG_TADDR1
+	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"1RECV");
+#elif CONFIG_TADDR2
+	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"2RECV");
+#elif CONFIG_TADDR3
+	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"3RECV");
+#elif CONFIG_TADDR4
+	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"4RECV");
+#elif CONFIG_TADDR5
+	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"5RECV");
+#endif
+	if (ret != ESP_OK) {
+		ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+	//Print settings
+	Nrf24_printDetails(&dev);
+
+	while(1) {
+		mydata.ivalue = (unsigned int)xTaskGetTickCount();
+		Nrf24_send(&dev, mydata.value);
+		vTaskDelay(1);
+		ESP_LOGI(pcTaskGetName(0), "Wait for sending.....");
+		if (Nrf24_isSend(&dev, 1000)) {
+			ESP_LOGI(pcTaskGetName(0),"Send success:%u", mydata.ivalue);
+		} else {
+			ESP_LOGW(pcTaskGetName(0),"Send fail:");
+		}
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+#endif // CONFIG_TRANSMITTER
+
 
 void app_main(void)
 {
+#if CONFIG_RECEIVER
 	xTaskCreate(receiver, "RECEIVER", 1024*3, NULL, 2, NULL);
+#endif
+
+#if CONFIG_TRANSMITTER
+    xTaskCreate(transmitter, "TRANSMITTER", 1024*3, NULL, 2, NULL);
+#endif
 }
