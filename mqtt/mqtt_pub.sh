78f5d716d1b85441efdd016928b4b7ab237bc880
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-07-27 18:57:51 +0900

    Update mqtt_pub.sh

--- mqtt/mqtt_pub.sh
+++ mqtt/mqtt_pub.sh
@@ -4,9 +4,10 @@ count=1
 while :
 do
         payload=`date "+%Y/%m/%d %H:%M:%S"`
-        #payload="text${count}"
+        #payload="count=${count}"
         echo ${payload}
         mosquitto_pub -h broker.emqx.io -p 1883 -t "/topic/mirf/test" -m "$payload"
+        count=$((++count))
         sleep 1
 done
 
