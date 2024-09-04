82a71474d4c3d23c73b8f1ad8056342450cb2730
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-09-04 16:20:56 +0900

    Changed the argument of pcTaskGetName

--- mqtt/main/main.c
+++ mqtt/main/main.c
@@ -192,21 +192,21 @@ void convert_mdns_host(char * from, char * to)
 void AdvancedSettings(NRF24_t * dev)
 {
 #if CONFIG_RF_RATIO_2M
-	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 2MBps");
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 2MBps");
 	Nrf24_SetSpeedDataRates(dev, 1);
 #endif // CONFIG_RF_RATIO_2M
 
 #if CONFIG_RF_RATIO_1M
-	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 1MBps");
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 1MBps");
 	Nrf24_SetSpeedDataRates(dev, 0);
 #endif // CONFIG_RF_RATIO_2M
 
 #if CONFIG_RF_RATIO_250K
-	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 250KBps");
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 250KBps");
 	Nrf24_SetSpeedDataRates(dev, 2);
 #endif // CONFIG_RF_RATIO_2M
 
-	ESP_LOGW(pcTaskGetName(0), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
+	ESP_LOGW(pcTaskGetName(NULL), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
 	Nrf24_setRetransmitDelay(dev, CONFIG_RETRANSMIT_DELAY);
 }
 #endif // CONFIG_ADVANCED
@@ -214,7 +214,7 @@ void AdvancedSettings(NRF24_t * dev)
 #if CONFIG_RECEIVER
 void receiver(void *pvParameters)
 {
-	ESP_LOGI(pcTaskGetName(0), "Start");
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
 	NRF24_t dev;
 	Nrf24_init(&dev);
 	uint8_t payload = 32;
@@ -224,7 +224,7 @@ void receiver(void *pvParameters)
 	// Set my own address using 5 characters
 	esp_err_t ret = Nrf24_setRADDR(&dev, (uint8_t *)"FGHIJ");
 	if (ret != ESP_OK) {
-		ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
 		while(1) { vTaskDelay(1); }
 	}
 
@@ -267,7 +267,7 @@ void receiver(void *pvParameters)
 #if CONFIG_SENDER
 void sender(void *pvParameters)
 {
-	ESP_LOGI(pcTaskGetName(0), "Start");
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
 	NRF24_t dev;
 	Nrf24_init(&dev);
 	uint8_t payload = 32;
@@ -277,7 +277,7 @@ void sender(void *pvParameters)
 	// Set destination address using 5 characters
 	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"FGHIJ");
 	if (ret != ESP_OK) {
-		ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
 		while(1) { vTaskDelay(1); }
 	}
 
@@ -288,18 +288,18 @@ void sender(void *pvParameters)
 	// Print settings
 	Nrf24_printDetails(&dev);
 
-	ESP_LOGI(pcTaskGetName(0), "Wait for mqtt...");
+	ESP_LOGI(pcTaskGetName(NULL), "Wait for mqtt...");
 	uint8_t buf[xItemSize];
 	while(1) {
 		size_t received = xMessageBufferReceive(xMessageBufferRecv, buf, sizeof(buf), portMAX_DELAY);
 		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=%d", received);
 		Nrf24_send(&dev, buf);
-		vTaskDelay(1);
-		ESP_LOGI(pcTaskGetName(0), "Wait for sending.....");
+		//vTaskDelay(1);
+		ESP_LOGI(pcTaskGetName(NULL), "Wait for sending.....");
 		if (Nrf24_isSend(&dev, 1000)) {
-			ESP_LOGI(pcTaskGetName(0),"Send success");
+			ESP_LOGI(pcTaskGetName(NULL),"Send success");
 		} else {
-			ESP_LOGW(pcTaskGetName(0),"Send fail");
+			ESP_LOGW(pcTaskGetName(NULL),"Send fail");
 		}
 	}
 }
