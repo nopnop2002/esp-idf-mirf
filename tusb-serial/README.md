f0fb94115e1d01eaef7ab9b559b1d03195684b0a
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-09-05 08:16:56 +0900

    added tusb-serial project

---
+++ tusb-serial/README.md
@@ -0,0 +1,93 @@
+# tinyusb Example   
+This is nRF24L01 and tinyusb gateway application.   
+```
+            +------------+          +------------+           +------------+
+            |            |          |            |           |            |
+            | USB Writer |--(USB)-->|    ESP32   |--(SPI)--->|  nRF24L01  |==(Radio)==>
+            |            |          |            |           |            |
+            +------------+          +------------+           +------------+
+
+            +------------+           +------------+          +------------+
+            |            |           |            |          |            |
+==(Radio)==>|  nRF24L01  |--(SPI)--->|    ESP32   |--(USB)-->| USB Reader |
+            |            |           |            |          |            |
+            +------------+           +------------+          +------------+
+```
+
+# Hardware requirements
+1. ESP32-S2/S3 Development board   
+Because the ESP32-S2/S3 does support USB OTG.   
+
+2. USB Connector   
+I used this:   
+![usb-connector](https://user-images.githubusercontent.com/6020549/124848149-3714ba00-dfd7-11eb-8344-8b120790c5c5.JPG)
+
+```
+ESP32-S2/S3 BOARD          USB CONNECTOR
+                           +--+
+                           | || VCC
+    [GPIO 19]    --------> | || D-
+    [GPIO 20]    --------> | || D+
+    [  GND  ]    --------> | || GND
+                           +--+
+```
+
+
+# Configuration
+![config-top](https://github.com/user-attachments/assets/305dd670-682b-42e4-bd1e-8093ca9d9ff2)
+
+### USB to Radio
+Subscribe with USB and send to Radio.   
+ESP32 acts as USB Reader.   
+You can use this script as USB Writer.   
+```python3 ./write.py```
+
+```
+           +------------+          +------------+           +------------+
+           |            |          |            |           |            |
+           | USB Writer |--(USB)-->|    ESP32   |--(SPI)--->|  nRF24L01  |==(Radio)==>
+           |            |          |            |           |            |
+           +------------+          +------------+           +------------+
+```
+
+![config-radio-1](https://github.com/user-attachments/assets/0cbdb9e2-a897-44ca-afe7-7007be0f41f2)
+
+Communicate with Arduino Environment.   
+Run this sketch.   
+ArduinoCode\Ra01S_RX   
+
+
+### Radio to USB
+Receive from Radio and publish as USB.   
+ESP32 acts as USB Writer.   
+You can use this script as USB Reader.   
+```python3 ./read.py```
+
+```
+           +------------+           +------------+          +------------+
+           |            |           |            |          |            |
+==(LoRa)==>|  nRF24L01  |--(SPI)--->|    ESP32   |--(USB)-->| USB Reader |
+           |            |           |            |          |            |
+           +------------+           +------------+          +------------+
+```
+
+![config-radio-2](https://github.com/user-attachments/assets/af909869-b5e0-4038-acae-cfca06a44e9f)
+
+Communicate with Arduino Environment.   
+Run this sketch.   
+ArduinoCode\Ra01S_TX   
+
+
+# Using Windows Terminal Software
+When you connect the USB cable to the USB port on your Windows machine and build the firmware, a new COM port will appear.   
+Open a new COM port in the terminal software.   
+I used TeraTerm.   
+![tusb-TeraTerm](https://github.com/user-attachments/assets/b5eea94e-5228-45b4-bcad-81cce8c52479)
+
+# Using Linux Terminal Software
+When you connect the USB cable to the USB port on your Linux machine and build the firmware, a new /dev/tty device will appear.   
+Open a new tty device in the terminal software.   
+Most occasions, the device is /dev/ttyACM0.   
+I used screen.   
+![tusb-screen](https://github.com/user-attachments/assets/18a6e519-9250-4109-b05d-6bcd418bfb5b)
+
---
+++ tusb-serial/main/main.c
@@ -0,0 +1,270 @@
+/*	Mirf Example
+
+	This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+	Unless required by applicable law or agreed to in writing, this
+	software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+	CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/message_buffer.h"
+#include "tinyusb.h"
+#include "tusb_cdc_acm.h"
+#include "esp_log.h"
+
+#include "mirf.h"
+
+static const char *TAG = "MAIN";
+
+MessageBufferHandle_t xMessageBufferTrans;
+MessageBufferHandle_t xMessageBufferRecv;
+QueueHandle_t xQueueTinyusb;
+
+// The total number of bytes (not single messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+// The size, in bytes, required to hold each item in the message,
+size_t xItemSize = 32;
+
+void tinyusb_cdc_rx_callback(int itf, cdcacm_event_t *event)
+{
+	/* initialization */
+	size_t rx_size = 0;
+	ESP_LOGD(TAG, "CONFIG_TINYUSB_CDC_RX_BUFSIZE=%d", CONFIG_TINYUSB_CDC_RX_BUFSIZE);
+
+	/* read */
+	uint8_t buf[CONFIG_TINYUSB_CDC_RX_BUFSIZE];
+	esp_err_t ret = tinyusb_cdcacm_read(itf, buf, CONFIG_TINYUSB_CDC_RX_BUFSIZE, &rx_size);
+	if (ret == ESP_OK) {
+		ESP_LOGD(TAG, "Data from channel=%d rx_size=%d", itf, rx_size);
+		ESP_LOG_BUFFER_HEXDUMP(TAG, buf, rx_size, ESP_LOG_INFO);
+		for(int i=0;i<rx_size;i++) {
+			xQueueSendFromISR(xQueueTinyusb, &buf[i], NULL);
+		}
+	} else {
+		ESP_LOGE(TAG, "tinyusb_cdcacm_read error");
+	}
+}
+
+void tinyusb_cdc_line_state_changed_callback(int itf, cdcacm_event_t *event)
+{
+	int dtr = event->line_state_changed_data.dtr;
+	int rts = event->line_state_changed_data.rts;
+	ESP_LOGI(TAG, "Line state changed on channel %d: DTR:%d, RTS:%d", itf, dtr, rts);
+}
+
+
+#if CONFIG_ADVANCED
+void AdvancedSettings(NRF24_t * dev)
+{
+#if CONFIG_RF_RATIO_2M
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 2MBps");
+	Nrf24_SetSpeedDataRates(dev, 1);
+#endif // CONFIG_RF_RATIO_2M
+
+#if CONFIG_RF_RATIO_1M
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 1MBps");
+	Nrf24_SetSpeedDataRates(dev, 0);
+#endif // CONFIG_RF_RATIO_2M
+
+#if CONFIG_RF_RATIO_250K
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 250KBps");
+	Nrf24_SetSpeedDataRates(dev, 2);
+#endif // CONFIG_RF_RATIO_2M
+
+	ESP_LOGW(pcTaskGetName(NULL), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
+	Nrf24_setRetransmitDelay(dev, CONFIG_RETRANSMIT_DELAY);
+}
+#endif // CONFIG_ADVANCED
+
+#if CONFIG_RECEIVER
+void receiver(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	NRF24_t dev;
+	Nrf24_init(&dev);
+	uint8_t payload = 32;
+	uint8_t channel = CONFIG_RADIO_CHANNEL;
+	Nrf24_config(&dev, channel, payload);
+
+	// Set my own address using 5 characters
+	esp_err_t ret = Nrf24_setRADDR(&dev, (uint8_t *)"FGHIJ");
+	if (ret != ESP_OK) {
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+#if CONFIG_ADVANCED
+	AdvancedSettings(&dev);
+#endif // CONFIG_ADVANCED
+
+	// Print settings
+	Nrf24_printDetails(&dev);
+	ESP_LOGI(pcTaskGetName(NULL), "Listening...");
+
+	// Clear RX FiFo
+	uint8_t buf[xItemSize];
+	while(1) {
+		if (Nrf24_dataReady(&dev) == false) break;
+		Nrf24_getData(&dev, buf);
+	}
+
+	while(1) {
+		// Wait for received data
+		if (Nrf24_dataReady(&dev)) {
+			Nrf24_getData(&dev, buf);
+			ESP_LOGI(pcTaskGetName(NULL), "Got data:%s", buf);
+			//ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buf, payload, ESP_LOG_INFO);
+
+			size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferTrans );
+			ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+			size_t sended = xMessageBufferSend(xMessageBufferTrans, buf, payload, 100);
+			if (sended != payload) {
+				ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail payload=%d sended=%d", payload, sended);
+				break;
+			}
+		}
+		vTaskDelay(1); // Avoid WatchDog alerts
+	}
+	vTaskDelete(NULL);
+}
+
+void usb_tx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1261/62/68 is 255
+	uint8_t crlf[2] = { 0x0d, 0x0a };
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferTrans, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", received, received, buf);
+		tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, buf, received);
+		tinyusb_cdcacm_write_queue(TINYUSB_CDC_ACM_0, crlf, 2);
+		tinyusb_cdcacm_write_flush(TINYUSB_CDC_ACM_0, 0);
+	} // end while
+	vTaskDelete(NULL);
+}
+#endif // CONFIG_RECEIVER
+
+
+#if CONFIG_SENDER
+void sender(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	NRF24_t dev;
+	Nrf24_init(&dev);
+	uint8_t payload = 32;
+	uint8_t channel = CONFIG_RADIO_CHANNEL;
+	Nrf24_config(&dev, channel, payload);
+
+	// Set destination address using 5 characters
+	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"FGHIJ");
+	if (ret != ESP_OK) {
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+#if CONFIG_ADVANCED
+	AdvancedSettings(&dev);
+#endif // CONFIG_ADVANCED
+
+	// Print settings
+	Nrf24_printDetails(&dev);
+
+	uint8_t buf[xItemSize+1];
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRecv, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=%d", received);
+
+		buf[received] = 0;
+		Nrf24_send(&dev, buf);
+		//vTaskDelay(1);
+		ESP_LOGI(pcTaskGetName(NULL), "Wait for sending.....");
+		if (Nrf24_isSend(&dev, 1000)) {
+			ESP_LOGI(pcTaskGetName(NULL),"Send success:%s", buf);
+		} else {
+			ESP_LOGW(pcTaskGetName(NULL),"Send fail:");
+		}
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+
+void usb_rx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	char buffer[xItemSize];
+	int index = 0;
+	while(1) {
+		char ch;
+		if(xQueueReceive(xQueueTinyusb, &ch, portMAX_DELAY)) {
+			ESP_LOGD(pcTaskGetName(NULL), "ch=0x%x",ch);
+			if (ch == 0x0d || ch == 0x0a) {
+				if (index > 0) {
+					ESP_LOGI(pcTaskGetName(NULL), "[%.*s]", index, buffer);
+					size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferRecv );
+					ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+					size_t sended = xMessageBufferSend(xMessageBufferRecv, &buffer, index, 100);
+					if (sended != index) {
+						ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail index=%d sended=%d", index, sended);
+						break;
+					}
+					index = 0;
+				}
+			} else {
+				if (index == xItemSize) continue;
+				buffer[index++] = ch;
+			}
+		}
+	} // end while
+	vTaskDelete(NULL);
+}
+#endif // CONFIG_SENDER
+
+
+void app_main(void)
+{
+	ESP_LOGI(TAG, "USB initialization");
+	const tinyusb_config_t tusb_cfg = {
+		.device_descriptor = NULL,
+		.string_descriptor = NULL,
+		.external_phy = false,
+		.configuration_descriptor = NULL,
+	};
+	ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+
+	tinyusb_config_cdcacm_t acm_cfg = {
+		.usb_dev = TINYUSB_USBDEV_0,
+		.cdc_port = TINYUSB_CDC_ACM_0,
+		.rx_unread_buf_sz = 64,
+		.callback_rx = &tinyusb_cdc_rx_callback, // the first way to register a callback
+		.callback_rx_wanted_char = NULL,
+		.callback_line_state_changed = &tinyusb_cdc_line_state_changed_callback,
+		.callback_line_coding_changed = NULL
+	};
+	ESP_ERROR_CHECK(tusb_cdc_acm_init(&acm_cfg));
+
+	// Create MessageBuffer
+	xMessageBufferTrans = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTrans );
+	xMessageBufferRecv = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRecv );
+
+	// Create Queue
+	xQueueTinyusb = xQueueCreate(100, sizeof(char));
+	configASSERT( xQueueTinyusb );
+
+#if CONFIG_RECEIVER
+	xTaskCreate(&receiver, "RADIO_RX", 1024*3, NULL, 5, NULL);
+	xTaskCreate(&usb_tx, "USB_TX", 1024*4, NULL, 5, NULL);
+#endif
+
+#if CONFIG_SENDER
+	xTaskCreate(&sender, "RADIO_TX", 1024*3, NULL, 5, NULL);
+	xTaskCreate(&usb_rx, "USB_RX", 1024*4, NULL, 5, NULL);
+#endif
+
+}
