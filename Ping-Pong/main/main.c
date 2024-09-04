d98b48d29a5abea1d81ff2fb36173889c47c2bb6
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-09-04 16:22:58 +0900

    Changed the argument of pcTaskGetName

--- Ping-Pong/main/main.c
+++ Ping-Pong/main/main.c
@@ -21,21 +21,21 @@
 void AdvancedSettings(NRF24_t * dev)
 {
 #if CONFIG_RF_RATIO_2M
-	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 2MBps");
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 2MBps");
 	Nrf24_SetSpeedDataRates(dev, 1);
 #endif // CONFIG_RF_RATIO_2M
 
 #if CONFIG_RF_RATIO_1M
-	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 1MBps");
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 1MBps");
 	Nrf24_SetSpeedDataRates(dev, 0);
 #endif // CONFIG_RF_RATIO_2M
 
 #if CONFIG_RF_RATIO_250K
-	ESP_LOGW(pcTaskGetName(0), "Set RF Data Ratio to 250KBps");
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 250KBps");
 	Nrf24_SetSpeedDataRates(dev, 2);
 #endif // CONFIG_RF_RATIO_2M
 
-	ESP_LOGW(pcTaskGetName(0), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
+	ESP_LOGW(pcTaskGetName(NULL), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
 	Nrf24_setRetransmitDelay(dev, CONFIG_RETRANSMIT_DELAY);
 }
 #endif // CONFIG_ADVANCED
@@ -43,7 +43,7 @@ void AdvancedSettings(NRF24_t * dev)
 #if CONFIG_PRIMARY
 void primary(void *pvParameters)
 {
-	ESP_LOGI(pcTaskGetName(0), "Start");
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
 	NRF24_t dev;
 	Nrf24_init(&dev);
 	uint8_t payload = 32;
@@ -53,14 +53,14 @@ void primary(void *pvParameters)
 	// Set my own address using 5 characters
 	esp_err_t ret = Nrf24_setRADDR(&dev, (uint8_t *)"ABCDE");
 	if (ret != ESP_OK) {
-		ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
 		while(1) { vTaskDelay(1); }
 	}
 
 	// Set destination address using 5 characters
 	ret = Nrf24_setTADDR(&dev, (uint8_t *)"FGHIJ");
 	if (ret != ESP_OK) {
-		ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
 		while(1) { vTaskDelay(1); }
 	}
 
@@ -77,12 +77,12 @@ void primary(void *pvParameters)
 		sprintf((char *)buf, "Hello World %"PRIu32, nowTick);
 		Nrf24_send(&dev, buf);
 		memset(buf, 0, sizeof(buf));
-		ESP_LOGI(pcTaskGetName(0), "Wait for sending.....");
+		ESP_LOGI(pcTaskGetName(NULL), "Wait for sending.....");
 		if (Nrf24_isSend(&dev, 1000)) {
-			ESP_LOGI(pcTaskGetName(0),"Send success:%s", buf);
+			ESP_LOGI(pcTaskGetName(NULL),"Send success:%s", buf);
 
 			// Wait for response
-			//ESP_LOGI(pcTaskGetName(0), "Wait for response.....");
+			//ESP_LOGI(pcTaskGetName(NULL), "Wait for response.....");
 			bool received = false;
 			for(int i=0;i<100;i++) {
 				if (Nrf24_dataReady(&dev)) {
@@ -94,13 +94,13 @@ void primary(void *pvParameters)
 			if (received) {
 				Nrf24_getData(&dev, buf);
 				TickType_t diffTick = xTaskGetTickCount() - nowTick;
-				ESP_LOGI(pcTaskGetName(0), "Got response:[%s] Elapsed:%"PRIu32" ticks", buf, diffTick);
+				ESP_LOGI(pcTaskGetName(NULL), "Got response:[%s] Elapsed:%"PRIu32" ticks", buf, diffTick);
 			} else {
-				ESP_LOGW(pcTaskGetName(0), "No response");
+				ESP_LOGW(pcTaskGetName(NULL), "No response");
 			}
 
 		} else {
-			ESP_LOGW(pcTaskGetName(0),"Send fail:");
+			ESP_LOGW(pcTaskGetName(NULL),"Send fail:");
 		}
 		vTaskDelay(1000/portTICK_PERIOD_MS);
 	}
@@ -110,7 +110,7 @@ void primary(void *pvParameters)
 #if CONFIG_SECONDARY
 void secondary(void *pvParameters)
 {
-	ESP_LOGI(pcTaskGetName(0), "Start");
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
 	NRF24_t dev;
 	Nrf24_init(&dev);
 	uint8_t payload = 32;
@@ -120,14 +120,14 @@ void secondary(void *pvParameters)
 	// Set my own address using 5 characters
 	esp_err_t ret = Nrf24_setRADDR(&dev, (uint8_t *)"FGHIJ");
 	if (ret != ESP_OK) {
-		ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
 		while(1) { vTaskDelay(1); }
 	}
 
 	// Set destination address using 5 characters
 	ret = Nrf24_setTADDR(&dev, (uint8_t *)"ABCDE");
 	if (ret != ESP_OK) {
-		ESP_LOGE(pcTaskGetName(0), "nrf24l01 not installed");
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
 		while(1) { vTaskDelay(1); }
 	}
 
@@ -137,7 +137,7 @@ void secondary(void *pvParameters)
 
 	// Print settings
 	Nrf24_printDetails(&dev);
-	ESP_LOGI(pcTaskGetName(0), "Listening...");
+	ESP_LOGI(pcTaskGetName(NULL), "Listening...");
 
 	uint8_t buf[32];
 
@@ -151,13 +151,13 @@ void secondary(void *pvParameters)
 		// Wait for received data
 		if (Nrf24_dataReady(&dev)) {
 			Nrf24_getData(&dev, buf);
-			ESP_LOGI(pcTaskGetName(0), "Got data:%s", buf);
+			ESP_LOGI(pcTaskGetName(NULL), "Got data:%s", buf);
 			Nrf24_send(&dev, buf);
-			ESP_LOGI(pcTaskGetName(0), "Wait for sending.....");
+			ESP_LOGI(pcTaskGetName(NULL), "Wait for sending.....");
 			if (Nrf24_isSend(&dev, 1000)) {
-				ESP_LOGI(pcTaskGetName(0),"Send success:%s", buf);
+				ESP_LOGI(pcTaskGetName(NULL),"Send success:%s", buf);
 			} else {
-				ESP_LOGW(pcTaskGetName(0),"Send fail:");
+				ESP_LOGW(pcTaskGetName(NULL),"Send fail:");
 			}
 		}
 		vTaskDelay(1);
