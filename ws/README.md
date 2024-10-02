3276ffd4cff627a933289363362972f0433e4b0b
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-10-03 07:25:28 +0900

    Added ws project

---
+++ ws/README.md
@@ -0,0 +1,77 @@
+# WebSocket Example   
+This is nRF24L01 and WebSocket gateway application.   
+```
+            +-----------+              +-----------+              +-----------+
+            |           |              |           |              |           |
+            | Web Client|-(WebSocket)->|   ESP32   |----(SPI)---->| nRF24L01  |==(Radio)==>
+            |           |              |           |              |           |
+            +-----------+              +-----------+              +-----------+
+
+            +-----------+              +-----------+              +-----------+
+            |           |              |           |              |           |
+==(Radio)==>| nRF24L01  |----(SPI)---->|   ESP32   |-(WebSocket)->| Web Server|
+            |           |              |           |              |           |
+            +-----------+              +-----------+              +-----------+
+```
+
+
+
+# Configuration
+![config-top](https://github.com/user-attachments/assets/3953103c-466b-4167-a298-dec72ff7bdad)
+![config-app](https://github.com/user-attachments/assets/11ee35bd-3f59-4b95-9ab3-1c07241751ae)
+
+## WiFi Setting
+
+![config-wifi](https://github.com/user-attachments/assets/8654b6a8-9dbe-46f7-825c-0928b1a0e9e1)
+
+## Radio Setting
+
+### WebSocket to Radio
+Subscribe with WebSocket and send to Radio.   
+ESP32 acts as Web Server.   
+You can use ws-client.py as Web Client.   
+You need to install websockets from here.   
+https://github.com/python-websockets/websockets   
+
+```
+            +-----------+              +-----------+              +-----------+
+            |           |              |           |              |           |
+            | Web Client|-(WebSocket)->|   ESP32   |----(SPI)---->| nRF24L01  |==(Radio)==>
+            |           |              |           |              |           |
+            +-----------+              +-----------+              +-----------+
+```
+
+![config-radio-1](https://github.com/user-attachments/assets/ce32a476-e13a-4c83-85cd-cdd8719fe08b)
+
+
+
+### Radio to WebSocket
+Receive from Radio and publish as WebSocket.   
+ESP32 acts as Web Client.   
+You can use ws-server.py as Web Server.   
+You need to install websocket-server from here.   
+https://github.com/Pithikos/python-websocket-server   
+
+```
+            +-----------+              +-----------+              +-----------+
+            |           |              |           |              |           |
+==(Radio)==>| nRF24L01  |----(SPI)---->|   ESP32   |-(WebSocket)->| Web Server|
+            |           |              |           |              |           |
+            +-----------+              +-----------+              +-----------+
+```
+
+![config-radio-2](https://github.com/user-attachments/assets/5a8a44e3-7982-4795-bdc0-1da57fd7cb01)
+
+
+
+### Specifying an WebSocket Server   
+You can specify your WebSocket Server in one of the following ways:   
+- IP address   
+ ```192.168.10.20```   
+- mDNS host name   
+ ```http-server.local```   
+
+
+# Communicate with Arduino Environment
+Run this sketch.   
+ArduinoCode\Peer-to-peer\StringTest   
---
+++ ws/main/main.c
@@ -0,0 +1,366 @@
+/*	Mirf Example
+
+	This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+	Unless required by applicable law or agreed to in writing, this
+	software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+	CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <inttypes.h>
+//#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/message_buffer.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "mdns.h"
+
+#include "mirf.h"
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about one event */
+/* - are we connected to the AP with an IP? */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static const char *TAG = "MAIN";
+
+static int s_retry_num = 0;
+
+MessageBufferHandle_t xMessageBufferTrans;
+MessageBufferHandle_t xMessageBufferRecv;
+
+// The total number of bytes (not single messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+// The size, in bytes, required to hold each item in the message,
+size_t xItemSize = 256;
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			s_retry_num++;
+			ESP_LOGI(TAG, "retry to connect to the AP");
+		} else {
+			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+		}
+		ESP_LOGI(TAG,"connect to the AP fail");
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+		s_retry_num = 0;
+		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+}
+
+void wifi_init_sta(void)
+{
+	s_wifi_event_group = xEventGroupCreate();
+
+	ESP_ERROR_CHECK(esp_netif_init());
+
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_create_default_wifi_sta();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	esp_event_handler_instance_t instance_any_id;
+	esp_event_handler_instance_t instance_got_ip;
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+		ESP_EVENT_ANY_ID,
+		&event_handler,
+		NULL,
+		&instance_any_id));
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+		IP_EVENT_STA_GOT_IP,
+		&event_handler,
+		NULL,
+		&instance_got_ip));
+
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = CONFIG_ESP_WIFI_SSID,
+			.password = CONFIG_ESP_WIFI_PASSWORD,
+			/* Setting a password implies station will connect to all security modes including WEP/WPA.
+			 * However these modes are deprecated and not advisable to be used. Incase your Access point
+			 * doesn't support WPA2, these mode can be enabled by commenting below line */
+			.threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+			.pmf_cfg = {
+				.capable = true,
+				.required = false
+			},
+		},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+	ESP_ERROR_CHECK(esp_wifi_start() );
+
+	ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+		WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+		pdFALSE,
+		pdFALSE,
+		portMAX_DELAY);
+
+	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+	 * happened. */
+	if (bits & WIFI_CONNECTED_BIT) {
+		ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else if (bits & WIFI_FAIL_BIT) {
+		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else {
+		ESP_LOGE(TAG, "UNEXPECTED EVENT");
+	}
+
+	/* The event will not be processed after unregister */
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+	vEventGroupDelete(s_wifi_event_group);
+}
+
+esp_err_t query_mdns_host(const char * host_name, char *ip)
+{
+	ESP_LOGD(__FUNCTION__, "Query A: %s", host_name);
+
+	struct esp_ip4_addr addr;
+	addr.addr = 0;
+
+	esp_err_t err = mdns_query_a(host_name, 10000,	&addr);
+	if(err){
+		if(err == ESP_ERR_NOT_FOUND){
+			ESP_LOGW(__FUNCTION__, "%s: Host was not found!", esp_err_to_name(err));
+			return ESP_FAIL;
+		}
+		ESP_LOGE(__FUNCTION__, "Query Failed: %s", esp_err_to_name(err));
+		return ESP_FAIL;
+	}
+
+	ESP_LOGD(__FUNCTION__, "Query A: %s.local resolved to: " IPSTR, host_name, IP2STR(&addr));
+	sprintf(ip, IPSTR, IP2STR(&addr));
+	return ESP_OK;
+}
+
+void convert_mdns_host(char * from, char * to)
+{
+	ESP_LOGI(__FUNCTION__, "from=[%s]",from);
+	strcpy(to, from);
+	char *sp;
+	sp = strstr(from, ".local");
+	if (sp == NULL) return;
+
+	int _len = sp - from;
+	ESP_LOGD(__FUNCTION__, "_len=%d", _len);
+	char _from[128];
+	strcpy(_from, from);
+	_from[_len] = 0;
+	ESP_LOGI(__FUNCTION__, "_from=[%s]", _from);
+
+	char _ip[128];
+	esp_err_t ret = query_mdns_host(_from, _ip);
+	ESP_LOGI(__FUNCTION__, "query_mdns_host=%d _ip=[%s]", ret, _ip);
+	if (ret != ESP_OK) return;
+
+	strcpy(to, _ip);
+	ESP_LOGI(__FUNCTION__, "to=[%s]", to);
+}
+
+void initialize_mdns(void)
+{
+	//initialize mDNS
+	ESP_ERROR_CHECK( mdns_init() );
+	//set mDNS hostname (required if you want to advertise services)
+	ESP_ERROR_CHECK( mdns_hostname_set(CONFIG_MDNS_HOSTNAME) );
+	ESP_LOGI(TAG, "mdns hostname set to: [%s]", CONFIG_MDNS_HOSTNAME);
+
+#if 0
+	//set default mDNS instance name
+	ESP_ERROR_CHECK( mdns_instance_name_set("ESP32 with mDNS") );
+#endif
+}
+
+#if CONFIG_ADVANCED
+void AdvancedSettings(NRF24_t * dev)
+{
+#if CONFIG_RF_RATIO_2M
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 2MBps");
+	Nrf24_SetSpeedDataRates(dev, 1);
+#endif // CONFIG_RF_RATIO_2M
+
+#if CONFIG_RF_RATIO_1M
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 1MBps");
+	Nrf24_SetSpeedDataRates(dev, 0);
+#endif // CONFIG_RF_RATIO_2M
+
+#if CONFIG_RF_RATIO_250K
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 250KBps");
+	Nrf24_SetSpeedDataRates(dev, 2);
+#endif // CONFIG_RF_RATIO_2M
+
+	ESP_LOGW(pcTaskGetName(NULL), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
+	Nrf24_setRetransmitDelay(dev, CONFIG_RETRANSMIT_DELAY);
+}
+#endif // CONFIG_ADVANCED
+
+#if CONFIG_RECEIVER
+void receiver(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	NRF24_t dev;
+	Nrf24_init(&dev);
+	uint8_t payload = 32;
+	uint8_t channel = CONFIG_RADIO_CHANNEL;
+	Nrf24_config(&dev, channel, payload);
+
+	// Set my own address using 5 characters
+	esp_err_t ret = Nrf24_setRADDR(&dev, (uint8_t *)"FGHIJ");
+	if (ret != ESP_OK) {
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+#if CONFIG_ADVANCED
+	AdvancedSettings(&dev);
+#endif // CONFIG_ADVANCED
+
+	// Print settings
+	Nrf24_printDetails(&dev);
+	ESP_LOGI(pcTaskGetName(NULL), "Listening...");
+
+	uint8_t buf[32];
+
+	// Clear RX FiFo
+	while(1) {
+		if (Nrf24_dataReady(&dev) == false) break;
+		Nrf24_getData(&dev, buf);
+	}
+
+	while(1) {
+		// Wait for received data
+		if (Nrf24_dataReady(&dev)) {
+			Nrf24_getData(&dev, buf);
+			ESP_LOGI(pcTaskGetName(NULL), "Got data:%s", buf);
+			ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buf, payload, ESP_LOG_INFO);
+
+			int rxLen = strlen((char *)buf);
+			size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferTrans );
+			ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+			size_t sended = xMessageBufferSend(xMessageBufferTrans, buf, rxLen, 100);
+			if (sended != rxLen) {
+				ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail rxLen=%d sended=%d", rxLen, sended);
+				break;
+			}
+		}
+		vTaskDelay(1); // Avoid WatchDog alerts
+	} // end while
+	vTaskDelete(NULL);
+}
+#endif // CONFIG_RECEIVER
+
+
+#if CONFIG_SENDER
+void sender(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	NRF24_t dev;
+	Nrf24_init(&dev);
+	uint8_t payload = 32;
+	uint8_t channel = CONFIG_RADIO_CHANNEL;
+	Nrf24_config(&dev, channel, payload);
+
+	// Set destination address using 5 characters
+	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"FGHIJ");
+	if (ret != ESP_OK) {
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+#if CONFIG_ADVANCED
+	AdvancedSettings(&dev);
+#endif // CONFIG_ADVANCED
+
+	// Print settings
+	Nrf24_printDetails(&dev);
+
+	uint8_t buf[32];
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRecv, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=%d", received);
+		ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buf, payload, ESP_LOG_INFO);
+
+		Nrf24_send(&dev, buf);
+		//vTaskDelay(1);
+		ESP_LOGI(pcTaskGetName(NULL), "Wait for sending.....");
+		if (Nrf24_isSend(&dev, 1000)) {
+			ESP_LOGI(pcTaskGetName(NULL),"Send success:%s", buf);
+		} else {
+			ESP_LOGW(pcTaskGetName(NULL),"Send fail:");
+		}
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	} // end while
+}
+#endif // CONFIG_SENDER
+
+void ws_client(void *pvParameters);
+void ws_server(void *pvParameters);
+
+void app_main(void)
+{
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	// Initialize WiFi
+	wifi_init_sta();
+
+	// Create MessageBuffer
+	xMessageBufferTrans = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTrans );
+	xMessageBufferRecv = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRecv );
+
+	// Initialize mDNS
+	initialize_mdns();
+
+	// Get the local IP address
+	esp_netif_ip_info_t ip_info;
+	ESP_ERROR_CHECK(esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip_info));
+	char cparam0[64];
+	sprintf(cparam0, IPSTR, IP2STR(&ip_info.ip));
+	ESP_LOGI(TAG, "cparam0=[%s]", cparam0);
+
+#if CONFIG_RECEIVER
+	xTaskCreate(&receiver, "RX", 1024*3, NULL, 5, NULL);
+	xTaskCreate(&ws_client, "WS_CLIENT", 1024*4, NULL, 5, NULL);
+#endif
+
+#if CONFIG_SENDER
+	xTaskCreate(&sender, "TX", 1024*3, NULL, 5, NULL);
+	xTaskCreate(&ws_server, "WS_SERVER", 1024*4, (void *)cparam0, 5, NULL);
+#endif
+
+	while(1) {
+		vTaskDelay(10);
+	}
+}
+
