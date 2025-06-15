1ab45882b2e31960983332595718614676ebdb78
nopnop2002 <>
2025-06-15 09:32:18 +0900

    added vcp project

---
+++ vcp/main/main.c
@@ -0,0 +1,181 @@
+/*	Mirf Example
+
+	This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+	Unless required by applicable law or agreed to in writing, this
+	software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+	CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/message_buffer.h"
+#include "esp_log.h"
+
+#include "mirf.h"
+
+static const char *TAG = "MAIN";
+
+MessageBufferHandle_t xMessageBufferTx;
+MessageBufferHandle_t xMessageBufferRx;
+
+// The total number of bytes (not messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+// The size, in bytes, required to hold each item in the message,
+size_t xItemSize = 34; // Maximum Payload size of nRF24L01 is 32
+
+#if CONFIG_ADVANCED
+void AdvancedSettings(NRF24_t * dev)
+{
+#if CONFIG_RF_RATIO_2M
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 2MBps");
+	Nrf24_SetSpeedDataRates(dev, 1);
+#endif // CONFIG_RF_RATIO_2M
+
+#if CONFIG_RF_RATIO_1M
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 1MBps");
+	Nrf24_SetSpeedDataRates(dev, 0);
+#endif // CONFIG_RF_RATIO_2M
+
+#if CONFIG_RF_RATIO_250K
+	ESP_LOGW(pcTaskGetName(NULL), "Set RF Data Ratio to 250KBps");
+	Nrf24_SetSpeedDataRates(dev, 2);
+#endif // CONFIG_RF_RATIO_2M
+
+	ESP_LOGW(pcTaskGetName(NULL), "CONFIG_RETRANSMIT_DELAY=%d", CONFIG_RETRANSMIT_DELAY);
+	Nrf24_setRetransmitDelay(dev, CONFIG_RETRANSMIT_DELAY);
+}
+#endif // CONFIG_ADVANCED
+
+#if CONFIG_RECEIVER
+void receiver(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	NRF24_t dev;
+	Nrf24_init(&dev);
+	uint8_t payload = 32;
+	uint8_t channel = CONFIG_RADIO_CHANNEL;
+	Nrf24_config(&dev, channel, payload);
+
+	// Set my own address using 5 characters
+	esp_err_t ret = Nrf24_setRADDR(&dev, (uint8_t *)"FGHIJ");
+	if (ret != ESP_OK) {
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+#if CONFIG_ADVANCED
+	AdvancedSettings(&dev);
+#endif // CONFIG_ADVANCED
+
+	// Print settings
+	Nrf24_printDetails(&dev);
+	ESP_LOGI(pcTaskGetName(NULL), "Listening...");
+
+	uint8_t buf[xItemSize];
+
+	// Clear RX FiFo
+	while(1) {
+		if (Nrf24_dataReady(&dev) == false) break;
+		Nrf24_getData(&dev, buf);
+	}
+
+	while(1) {
+		// Wait for received data
+		if (Nrf24_dataReady(&dev)) {
+			Nrf24_getData(&dev, buf);
+			ESP_LOGI(pcTaskGetName(NULL), "Got data:%s", buf);
+			ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buf, payload, ESP_LOG_INFO);
+
+			int txLen = strlen((char *)buf);
+			ESP_LOGI(pcTaskGetName(NULL), "txLen=%d", txLen);
+			buf[txLen] = 0x0a;
+			txLen++;
+			size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferTx );
+			ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+			size_t sended = xMessageBufferSend(xMessageBufferTx, buf, txLen, 100);
+			if (sended != txLen) {
+				ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail txLen=%d sended=%d", txLen, sended);
+				break;
+			}
+			
+		}
+		vTaskDelay(1); // Avoid WatchDog alerts
+	}
+}
+#endif // CONFIG_RECEIVER
+
+
+#if CONFIG_SENDER
+void sender(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	NRF24_t dev;
+	Nrf24_init(&dev);
+	uint8_t payload = 32;
+	uint8_t channel = CONFIG_RADIO_CHANNEL;
+	Nrf24_config(&dev, channel, payload);
+
+	// Set destination address using 5 characters
+	esp_err_t ret = Nrf24_setTADDR(&dev, (uint8_t *)"FGHIJ");
+	if (ret != ESP_OK) {
+		ESP_LOGE(pcTaskGetName(NULL), "nrf24l01 not installed");
+		while(1) { vTaskDelay(1); }
+	}
+
+#if CONFIG_ADVANCED
+	AdvancedSettings(&dev);
+#endif // CONFIG_ADVANCED
+
+	// Print settings
+	Nrf24_printDetails(&dev);
+
+	uint8_t buf[xItemSize];
+	while(1) {
+		size_t received = xMessageBufferReceive(xMessageBufferRx, buf, sizeof(buf), portMAX_DELAY);
+		ESP_LOGI(pcTaskGetName(NULL), "xMessageBufferReceive received=%d", received);
+
+		// The VCP termination character is CR+LF. So Remove CR/LF.
+		for (int i=0;i<received;i++) {
+			if (buf[i] == 0x0d) buf[i] = 0;
+			if (buf[i] == 0x0a) buf[i] = 0;
+		}
+		ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buf, received, ESP_LOG_INFO);
+		Nrf24_send(&dev, buf);
+		//vTaskDelay(1);
+		ESP_LOGI(pcTaskGetName(NULL), "Wait for sending.....");
+		if (Nrf24_isSend(&dev, 1000)) {
+			ESP_LOGI(pcTaskGetName(NULL),"Send success:%s", buf);
+		} else {
+			ESP_LOGW(pcTaskGetName(NULL),"Send fail:");
+		}
+		vTaskDelay(1000/portTICK_PERIOD_MS);
+	}
+}
+#endif // CONFIG_SENDER
+
+void cdc_acm_vcp_task(void *pvParameters);
+
+void app_main(void)
+{
+	// Create MessageBuffer
+	xMessageBufferTx = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTx );
+	xMessageBufferRx = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRx );
+
+#if CONFIG_RECEIVER
+	xTaskCreate(&receiver, "RECEIVE", 1024*3, NULL, 2, NULL);
+#endif
+
+#if CONFIG_SENDER
+	xTaskCreate(&sender, "SEND", 1024*3, NULL, 2, NULL);
+#endif
+
+	// Start CDC_ACM_VCP
+	xTaskCreate(cdc_acm_vcp_task, "CDC_ACM_VCP", 1024*4, NULL, 2, NULL);
+}
