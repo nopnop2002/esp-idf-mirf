ef38bd4d080357fd7f188f1e9489913230692552
nopnop2002 <>
2025-06-16 12:57:24 +0900

    Changed the priority of a task

--- Peer-to-peer-indexed/main/main.c
+++ Peer-to-peer-indexed/main/main.c
@@ -134,11 +134,11 @@ void sender(void *pvParameters)
 void app_main(void)
 {
 #if CONFIG_RECEIVER
-	xTaskCreate(&receiver, "RECEIVER", 1024*3, NULL, 2, NULL);
+	xTaskCreate(&receiver, "RECEIVER", 1024*3, NULL, 5, NULL);
 #endif
 
 #if CONFIG_SENDER
-	xTaskCreate(&sender, "SENDER", 1024*3, NULL, 2, NULL);
+	xTaskCreate(&sender, "SENDER", 1024*3, NULL, 5, NULL);
 #endif
 
 }
